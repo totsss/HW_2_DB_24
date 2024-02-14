5ae15ae4aa5843a463f53b97843025b906fbb6f0
totsss <axetra@hotmail.com>
2024-02-14 09:22:24 +0000

    start 1.1

---
+++ HW2-TEMPLATE.sql
@@ -0,0 +1,58 @@
+-- HW2
+-- Student names:
+
+-- A. 447 different members attended at least one class on January 10th. How many different members attended at least one class on January 15th?
+-- Explanation: 
+
+
+
+-- B. 4 different class types require more than 20 light dumbbells. How many class types require more than 20 yoga mats?
+-- Explanation: 
+
+
+
+-- C. Oh no! Some member hacked the database and is still attending classes but has quit according to the database. Write a query to reveal their name!
+-- Explanation: 
+
+
+
+-- D. How many members have a personal trainer with the same first name as themselves, but have never attended a class that their personal trainer led?
+-- Explanation: 
+
+
+
+-- E. For every class type, return its name and whether it has an average rating higher or equal to 7, or lower than 7, in a column named "Rating" with values "Good" or "Bad", respectively.
+-- Explanation: 
+
+
+
+-- F. Out of the members that have not quit, member with ID 6976 has been a customer for the shortest time. Out of the members that have not quit, return the ID of the member(s) that have been customer(s) for the longest time.
+-- Explanation: 
+
+
+
+-- G. How many class types have at least one equipment that costs more than 100.000 and at least one other equipment that costs less than 5.000?
+-- Explanation: 
+
+
+
+-- H. How many instructors have led a class in all gyms on the same day?
+-- Explanation: 
+
+
+
+-- I. How many instructors have not led classes of all different class types?
+-- Explanation: 
+
+
+
+-- J. The class type "Circuit training" has the lowest equipment cost per member, based on full capacity. Return the name of the class type that has the highest equipment cost per person, based on full capacity.
+-- Explanation: 
+
+
+
+-- K (BONUS). The hacker revealed in query C has left a message for the database engineers. This message may save the database!
+-- Return the 5th letter of all members that started the gym on December 24th of any year and have at least 3 different odd numbers in their phone number, in a descending order of their IDs,
+-- followed by the 8th letter of all instructors that have not led any "Trampoline Burn" classes, in an ascending order of their IDs.
+-- Explanation: 
+
